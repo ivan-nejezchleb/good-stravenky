a1a8c0d6e364f9032b5946b02a25aa615219fb47
xMort <petr.dolejsi@gooddata.com>
2018-11-07 22:14:52 +0100

    styled welcome page

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,33 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
-import {
-    translate
-} from '../services/translationsService';
+import { translate } from '../services/translationsService';
 
 const styles = StyleSheet.create({
-    header: {
-        fontWeight: '800',
-        paddingTop: 40
-    },
     container: {
         flex: 1,
-        paddingTop: 15
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    header: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        paddingTop: 130,
+        color: '#778491'
+    },
+    tagLine: {
+        fontSize: 15,
+        color: '#778491',
+        textAlign: 'center',
+        paddingHorizontal: 60,
+        paddingVertical: 30
+    },
+    logo: {
+        marginVertical: 40
+    },
+    buttonWrapper: {
+        marginTop: 20,
+        borderWidth: 1,
+        borderRadius: 20,
+        borderColor: '#4A9AEA',
+        paddingHorizontal: 80,
+        paddingVertical: 10,
+        backgroundColor: '#4A9AEA'
+    },
+    button: {
+        color: 'white',
+        fontSize: 20
     }
 });
 
 export default class WelcomeScreen extends React.Component {
-  static navigationOptions = {
-      header: null
-  };
+    static navigationOptions = {
+        header: null
+    };
 
-  render() {
-      const { navigate } = this.props.navigation;
-      return (
-          <View style={styles.container}>
-              <Text>{translate('welcome.header')}</Text>
-              <Button onPress={() => navigate('Settings')} title={translate('welcome.settingsButton')} />
-          </View>
-      );
-  }
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.header}>{translate('welcome.header')}</Text>
+                </View>
+                <View>
+                    <Text style={styles.tagLine}>{translate('welcome.tagLine')}</Text>
+                </View>
+                <View style={styles.logo}>
+                    <Image source={require('./img/welcome_pic.png')} />
+                </View>
+                <View>
+                    <TouchableOpacity onPress={() => navigate('Settings')} style={styles.buttonWrapper} activeOpacity={0.8}>
+                        <Text style={styles.button}>
+                            {translate('welcome.settingsButton')}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
